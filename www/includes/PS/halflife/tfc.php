172501e85fc53da1f918f2c856f3b9e791ebb4de
Drek <doxology@rogers.com>
2021-06-24 21:39:30 -0300

    Reverted unintentional change, thanks Rosenstein.

--- www/includes/PS/halflife/tfc.php
+++ www/includes/PS/halflife/tfc.php
@@ -9,7 +9,7 @@ if (!defined("PSYCHOSTATS_PAGE")) die("Unauthorized access to " . basename(__FIL
 if (defined("CLASS_PS_HALFLIFE_TFC_PHP")) return 1;
 define("CLASS_PS_HALFLIFE_TFC_PHP", 1);
 
-include_once(dirname(__DIR__) . '/halflife.php');
+include_once(rtrim(dirname(__DIR__), '/\\') . '/halflife.php');
 
 class PS_halflife_tfc extends PS_halflife {
 
